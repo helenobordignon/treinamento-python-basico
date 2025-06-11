d0e636ef82387fdd81580e7d61746c5f3f9e937f
Heleno Bordignon <helenobordignon.qa@gmail.com>
2025-06-10 22:08:34 -0300

    basico-1

--- treinamento/1-basico/variaveis.py
+++ treinamento/1-basico/variaveis.py
@@ -39,3 +39,32 @@ def imprimir_dados(texto, numero):
  
 # Passo 2: Chamar a função imprimir_dados com dois argumentos
 imprimir_dados("Ano: ", 2025) 
+
+
+# Variáveis locais e globais:
+# As variáveis locais são aquelas definidas dentro de uma função e só podem ser acessadas dentro dessa função.
+# As variáveis globais são aquelas definidas fora de qualquer função e podem ser acessadas em qualquer parte do código.
+
+# Exemplo de variáveis locais e globais:
+def multiplicador(numero):
+        a = 2 # esta variável tem escopo local
+        print(f"Dentro da função, a variável vale: {a}")
+        return a * numero
+
+a = 3 # esta variável tem escopo global
+b = multiplicador(5)
+print(f"Fora da função, a variável a vale: {a}")
+
+# Constantes
+
+# Em Python, não há uma maneira nativa de definir constantes como em outras linguagens, mas é uma convenção usar letras maiúsculas para indicar que uma variável deve ser tratada como constante.
+PI = 3.14159  # Constante para o valor de pi
+
+# Operadores matemáticos
+
+# <	    Menor que
+# <=	Menor ou igual a
+# >	    Maior que
+# >=	Maior ou igual a
+# ==	Igual
+# !=	Não igual
