e7cc42aa2300d64fd05bf023bccf11e2a1e42547
Heleno Bordignon <135024897+helenobordignon@users.noreply.github.com>
2025-09-29 22:19:40 -0300

    exercícios 001 e 002

---
+++ treinamento/1-python_basico/exercicios/001.py
@@ -0,0 +1,7 @@
+# imprimir no conlose "Olá, Mundo!"
+print('#1 --> Olá, Mundo!')
+
+msg = '#2 --> Olá, Mundo!'
+print(msg)
+
+
---
+++ treinamento/1-python_basico/exercicios/002.py
@@ -0,0 +1,3 @@
+# Entrada de dados do usuário:
+nome = input("Digite seu nome: ")
+print("Olá, " + nome + "!")
